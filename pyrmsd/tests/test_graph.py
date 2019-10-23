542126879a9de763eb7aefa1cb616272a86a2036
Rocco Meli <r.meli@bluemail.ch>
2019-10-23 12:04:11 +0100

    test named graph

--- pyrmsd/tests/test_graph.py
+++ pyrmsd/tests/test_graph.py
@@ -2,7 +2,7 @@ from pyrmsd import molecule, graph
 from pyrmsd.tests import molecules
 
 
-def test_graph_from_molecule_benzene_xyz():
+def test_graph_from_molecule_benzene():
 
     mol = molecules.benzene
 
@@ -14,7 +14,25 @@ def test_graph_from_molecule_benzene_xyz():
     assert G.number_of_edges() == 12
 
 
-def test_graph_from_molecule_ethanol_xyz():
+def test_graph_from_molecule_named_benzene():
+
+    mol = molecules.benzene
+
+    m = molecule.openbabel_to_molecule(mol)
+
+    G = graph.graph_from_molecule(m.atomicnums, m.coordinates, named=True)
+
+    assert G.number_of_nodes() == len(m)
+    assert G.number_of_edges() == 12
+
+    nodes = G.nodes()
+    for node in nodes:
+        element = nodes[node]["element"]
+
+        assert element == "H" or element == "C"
+
+
+def test_graph_from_molecule_ethanol():
 
     mol = molecules.ethanol
 
@@ -26,7 +44,7 @@ def test_graph_from_molecule_ethanol_xyz():
     assert G.number_of_edges() == 8
 
 
-def test_graph_from_molecule_dialanine_sdf():
+def test_graph_from_molecule_dialanine():
 
     mol = molecules.dialanine
 
