6d8fed3448a0e46fa53fc3ec915cb3cd97b785d1
Rocco Meli <r.meli@bluemail.ch>
2019-11-02 16:40:26 +0000

    test hungarian refactor

--- pyrmsd/tests/test_hungarian.py
+++ pyrmsd/tests/test_hungarian.py
@@ -1,4 +1,4 @@
-from pyrmsd import hungarian
+from pyrmsd import hungarian, molecule
 from pyrmsd.tests import molecules
 
 import pytest
@@ -7,46 +7,46 @@ import numpy as np
 import copy
 
 
-def test_cost_mtx():
+@pytest.mark.parametrize("mol", molecules.allmolecules)
+def test_cost_mtx(mol: molecule.Molecule):
 
-    for mol in molecules.allmolecules:
-        mol1 = copy.deepcopy(mol)
-        mol2 = copy.deepcopy(mol)
+    mol1 = copy.deepcopy(mol)
+    mol2 = copy.deepcopy(mol)
 
-        M = hungarian.cost_mtx(mol1.coordinates, mol2.coordinates)
+    M = hungarian.cost_mtx(mol1.coordinates, mol2.coordinates)
 
-        n = len(mol1)
+    n = len(mol1)
 
-        assert len(mol2) == n
-        assert M.shape == (n, n)
-        assert np.all(M >= 0)
+    assert len(mol2) == n
+    assert M.shape == (n, n)
+    assert np.all(M >= 0)
 
-        for i in range(n):
-            for j in range(n):
-                a, b = mol1.coordinates[i, :], mol2.coordinates[j, :]
-                ab = b - a
-                assert M[i, j] == pytest.approx(np.dot(ab, ab))
+    for i in range(n):
+        for j in range(n):
+            a, b = mol1.coordinates[i, :], mol2.coordinates[j, :]
+            ab = b - a
+            assert M[i, j] == pytest.approx(np.dot(ab, ab))
 
 
-def test_optimal_assignement_same_molecule():
+@pytest.mark.parametrize("mol", molecules.allmolecules)
+def test_optimal_assignement_same_molecule(mol: molecule.Molecule):
 
-    for mol in molecules.allmolecules:
-        mol1 = copy.deepcopy(mol)
-        mol2 = copy.deepcopy(mol)
+    mol1 = copy.deepcopy(mol)
+    mol2 = copy.deepcopy(mol)
 
-        assert len(mol1) == len(mol2)
+    assert len(mol1) == len(mol2)
 
-        n = len(mol1)
+    n = len(mol1)
 
-        cost, row_idx, col_idx = hungarian.optimal_assignment(
-            mol1.coordinates, mol2.coordinates
-        )
+    cost, row_idx, col_idx = hungarian.optimal_assignment(
+        mol1.coordinates, mol2.coordinates
+    )
 
-        # When molecules are the same, the assignment is perfect and the cost is zero
-        assert cost == pytest.approx(0)
+    # When molecules are the same, the assignment is perfect and the cost is zero
+    assert cost == pytest.approx(0)
 
-        assert len(row_idx) == len(col_idx) == n
+    assert len(row_idx) == len(col_idx) == n
 
-        # The perfect assignment corresponds to the original ordering
-        assert np.all(row_idx == np.arange(0, n))
-        assert np.all(col_idx == np.arange(0, n))
+    # The perfect assignment corresponds to the original ordering
+    assert np.all(row_idx == np.arange(0, n))
+    assert np.all(col_idx == np.arange(0, n))
