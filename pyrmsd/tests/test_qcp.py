6c2edd8e1214970d47ee452c399e191fe5947820
Rocco Meli <r.meli@bluemail.ch>
2019-11-02 17:22:01 +0000

    test qcp refactoring

--- pyrmsd/tests/test_qcp.py
+++ pyrmsd/tests/test_qcp.py
@@ -1,4 +1,4 @@
-from pyrmsd import qcp
+from pyrmsd import qcp, molecule
 from pyrmsd.tests import molecules
 
 import numpy as np
@@ -7,60 +7,59 @@ import copy
 
 import pytest
 
-
-def test_M_mtx():
-    for mol in molecules.allmolecules:
-        mol1 = copy.deepcopy(mol)
-        mol2 = copy.deepcopy(mol)
-
-        # Build rotated coordinate set
-        mol2.rotate(10, np.random.rand(3))
-
-        M = qcp.M_mtx(mol1.coordinates, mol2.coordinates)
-
-        assert M.shape == (3, 3)
-
-        def S(i, j):
-            A, B = mol1.coordinates, mol2.coordinates
-            return np.dot(B[:, i], A[:, j])
-
-        for i, j in itertools.combinations(range(3), 2):
-            assert M[i, j] == pytest.approx(S(i, j))
+from typing import Tuple
 
 
-def test_K_mtx():
+@pytest.mark.parametrize("mol", molecules.allmolecules)
+def test_M_mtx(mol: molecule.Molecule) -> None:
+    mol1 = copy.deepcopy(mol)
+    mol2 = copy.deepcopy(mol)
 
-    for mol in molecules.allmolecules:
-        mol1 = copy.deepcopy(mol)
-        mol2 = copy.deepcopy(mol)
+    # Build rotated coordinate set
+    mol2.rotate(10, np.random.rand(3))
 
-        # Build rotated coordinate set
-        mol2.rotate(10, np.random.rand(3))
+    M = qcp.M_mtx(mol1.coordinates, mol2.coordinates)
 
-        M = qcp.M_mtx(mol1.coordinates, mol2.coordinates)
-        K = qcp.K_mtx(M)
+    assert M.shape == (3, 3)
 
-        assert K.shape == (4, 4)
-        assert np.allclose(K.T, K)
-        assert np.trace(K) == pytest.approx(0)
+    def S(i, j):
+        A, B = mol1.coordinates, mol2.coordinates
+        return np.dot(B[:, i], A[:, j])
+
+    for i, j in itertools.combinations(range(3), 2):
+        assert M[i, j] == pytest.approx(S(i, j))
 
 
-def test_lambda_max():
+@pytest.mark.parametrize("mol", molecules.allmolecules)
+def test_K_mtx(mol: molecule.Molecule) -> None:
 
-    # f(x) = x^4 - 1; x_0 = 2
-    assert qcp.lambda_max(2, 2, 0, 0, -1) == pytest.approx(1)
+    mol1 = copy.deepcopy(mol)
+    mol2 = copy.deepcopy(mol)
 
-    # f(x) = x^4 - 1; x_0 = -2
-    assert qcp.lambda_max(-2, -2, 0, 0, -1) == pytest.approx(-1)
+    # Build rotated coordinate set
+    mol2.rotate(10, np.random.rand(3))
 
-    # f(x) = x^4 - 5 * x^2 + 4; x_0 = 3
-    assert qcp.lambda_max(3, 3, -5, 0, 4) == pytest.approx(2)
+    M = qcp.M_mtx(mol1.coordinates, mol2.coordinates)
+    K = qcp.K_mtx(M)
 
-    # f(x) = x^4 - 5 * x^2 + 4; x_0 = 1/2
-    assert qcp.lambda_max(1, 1, -5, 0, 4) == pytest.approx(1)
+    assert K.shape == (4, 4)
+    assert np.allclose(K.T, K)
+    assert np.trace(K) == pytest.approx(0)
 
-    # f(x) = x^4 - 5 * x^2 + 4; x_0 = -1/2
-    assert qcp.lambda_max(-1, -1, -5, 0, 4) == pytest.approx(-1)
 
-    # f(x) = x^4 - 5 * x^2 + 4; x_0 = -3
-    assert qcp.lambda_max(-3, -3, -5, 0, 4) == pytest.approx(-2)
+@pytest.mark.parametrize(
+    "input, result",
+    [
+        ((2, 2, 0, 0, -1), 1),  # f(x) = x^4 - 1; x_0 = 2
+        ((-2, -2, 0, 0, -1), -1),  # f(x) = x^4 - 1; x_0 = -2
+        ((3, 3, -5, 0, 4), 2),  # f(x) = x^4 - 5 * x^2 + 4; x_0 = 3
+        ((1, 1, -5, 0, 4), 1),  # f(x) = x^4 - 5 * x^2 + 4; x_0 = 1/2
+        ((-1, -1, -5, 0, 4), -1),  # f(x) = x^4 - 5 * x^2 + 4; x_0 = -1/2
+        ((-3, -3, -5, 0, 4), -2),  # f(x) = x^4 - 5 * x^2 + 4; x_0 = -3
+    ],
+)
+def test_lambda_max(
+    input: Tuple[float, float, float, float, float], result: float
+) -> None:
+
+    assert qcp.lambda_max(*input) == pytest.approx(result)
