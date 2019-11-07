fdf3604e9df039d0e37b97a058ec545d06e2d06b
Rocco Meli <r.meli@bluemail.ch>
2019-11-07 11:50:08 +0000

    doc and type annotation in rmsd

--- pyrmsd/rmsd.py
+++ pyrmsd/rmsd.py
@@ -1,9 +1,35 @@
-from pyrmsd import qcp, hungarian, graph
+from pyrmsd import qcp, hungarian, graph, molecule
 
 import numpy as np
 
 
-def rmsd_dummy(mol1, mol2, center=False):
+def rmsd_dummy(
+    mol1: molecule.Molecule, mol2: molecule.Molecule, center: bool = False
+) -> float:
+    """
+    Compute dummy (naïve) RMSD.
+
+    Parameters
+    ----------
+    mol1: molecule.Molecule
+        Molecule 1
+    mol2: molecule.Molecule
+        Molecule 2
+    center: bool
+        Flag for centering the molecules at the origin
+
+    Returns
+    -------
+    float
+        Dummy (naïve) RMSD
+
+    Notes
+    -----
+    The dummy (naïve) RMSD distribution assumes a one-to-one mapping of the atoms in
+    the order they are stored.
+
+    The equality between all the atomic numbers is checked for sanity.
+    """
 
     assert np.all(mol1.atomicnums == mol2.atomicnums)
     assert mol1.coordinates.shape == mol2.coordinates.shape
@@ -20,7 +46,30 @@ def rmsd_dummy(mol1, mol2, center=False):
     return np.sqrt(np.sum((c1 - c2) ** 2) / n)
 
 
-def rmsd_qcp(mol1, mol2):
+def rmsd_qcp(mol1, mol2) -> float:
+    """
+    Compute minimum RMSD using the Quaternion Characteristic Polynomial method.
+
+    Parameters
+    ----------
+    mol1: molecule.Molecule
+        Molecule 1
+    mol2: molecule.Molecule
+        Molecule 2
+
+    Returns
+    -------
+    float
+        Minimum RMSD (after superimposition)
+
+    Notes
+    -----
+    The molecules are always centred at the origin according to the center of geometry
+    and superimposed in order to minimize the RMSD. See [1]_ for details.
+
+    .. [1] D. L. Theobald, *Rapid calculation of RMSDs using a quaternion-based
+       characteristic polynomial*, Acta Crys. A**61**, 478-480 (2005).
+    """
 
     assert np.all(mol1.atomicnums == mol2.atomicnums)
 
@@ -31,6 +80,32 @@ def rmsd_qcp(mol1, mol2):
 
 
 def rmsd_hungarian(mol1, mol2, center=False):
+    """
+    Compute minimum RMSD using the Hungarian method.
+
+    Parameters
+    ----------
+    mol1: molecule.Molecule
+        Molecule 1
+    mol2: molecule.Molecule
+        Molecule 2
+
+    Returns
+    -------
+    float
+        Minimum RMSD (after assignment)
+
+    Notes
+    -----
+    The Hungarian algorithm is used to solve the linear assignment problem, which is
+    a minimum weight matching of the molecular graphs (bipartite). See [2]_ for details.
+
+    The linear assignment problem is solved for every element separately.
+
+    .. [2] W. J. Allen and R. C. Rizzo, *Implementation of the Hungarian Algorithm to
+        Account for Ligand Symmetry and Similarity in Structure-Based Design*,
+        J. Chem. Inf. Model. **54**, 518-529 (2014)
+    """
 
     assert mol1.atomicnums.shape == mol2.atomicnums.shape
     assert mol1.coordinates.shape == mol2.coordinates.shape
@@ -46,6 +121,21 @@ def rmsd_hungarian(mol1, mol2, center=False):
 
 
 def rmsd_isomorphic(mol1, mol2, center=False):
+    """
+    Compute minimum RMSD using graph isomorphism.
+
+    Parameters
+    ----------
+    mol1: molecule.Molecule
+        Molecule 1
+    mol2: molecule.Molecule
+        Molecule 2
+
+    Returns
+    -------
+    float
+        Minimum RMSD (after graph matching)
+    """
 
     assert mol1.atomicnums.shape == mol2.atomicnums.shape
     assert mol1.coordinates.shape == mol2.coordinates.shape
@@ -59,13 +149,17 @@ def rmsd_isomorphic(mol1, mol2, center=False):
         c1 -= mol1.center_of_geometry()
         c2 -= mol2.center_of_geometry()
 
+    # Convert molecules to graphs
     G1 = mol1.to_graph()
     G2 = mol2.to_graph()
 
+    # Get all the possible graph isomorphisms
     isomorphisms = graph.match_graphs(G1, G2)
 
     # Minimum squared displacement
     min_sd = np.inf
+
+    # Loop over all graph isomorphisms to find the lowest RMSD
     for isomorphism in isomorphisms:
 
         # Use the isomorphism to shuffle coordinates around (from original order)
@@ -79,4 +173,5 @@ def rmsd_isomorphic(mol1, mol2, center=False):
         if sd < min_sd:
             min_sd = sd
 
+    # Return the actual RMSD
     return np.sqrt(min_sd / n)
