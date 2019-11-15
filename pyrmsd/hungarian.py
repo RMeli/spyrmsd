55ae180a4467b330c25043d19f4a1f2a35ce7ece
Rocco Meli <r.meli@bluemail.ch>
2019-11-15 14:37:44 +0000

    better comments for hungarian algorithm

--- pyrmsd/hungarian.py
+++ pyrmsd/hungarian.py
@@ -2,12 +2,44 @@ import numpy as np
 import scipy
 
 
-def cost_mtx(A, B):
+def cost_mtx(A: np.ndarray, B: np.ndarray):
+    """
+    Compute the cost matrix for atom-atom assignment.
+
+    Parameters
+    ----------
+    A: numpy.ndarray
+        Atomic coordinates of molecule A
+    B: numpy.ndarray
+        Atomic coordinates of molecule B
+
+    Returns
+    -------
+    np.ndarray
+        Cost matrix of squared atomic distances between atoms of molecules A and B
+    """
 
     return scipy.spatial.distance.cdist(A, B) ** 2
 
 
-def optimal_assignment(A, B):
+def optimal_assignment(A: np.ndarray, B: np.ndarray):
+    """
+    Solve the optimal assignement problems between atomic coordinates of molecules A and
+    B.
+
+    Parameters
+    ----------
+    A: numpy.ndarray
+        Atomic coordinates of molecule A
+    B: numpy.ndarray
+        Atomic coordinates of molecule B
+
+    Returns
+    -------
+    Tuple[float, nd.array, nd.array]
+        Cost of the optimal assignment, together with the row and colum indices of said
+        assignment
+    """
 
     C = cost_mtx(A, B)
 
@@ -19,21 +51,56 @@ def optimal_assignment(A, B):
     return cost, row_idx, col_idx
 
 
-def hungarian_rmsd(A, B, typesA, typesB):
+def hungarian_rmsd(
+    A: np.ndarray, B: np.ndarray, atomicnumsA: np.ndarray, atomicnumsB: np.ndarray
+) -> float:
+    """
+    Solve the optimal assignement problems between atomic coordinates of molecules A and
+    B.
+
+    Parameters
+    ----------
+    A: numpy.ndarray
+        Atomic coordinates of molecule A
+    B: numpy.ndarray
+        Atomic coordinates of molecule B
+    atomicnumsA: numpy.ndarray
+        Atomic numbers of molecule A
+    atomicnumsB: numpy.ndarray
+        Atomic numbers of molecule B
+
+    Returns
+    -------
+    float
+        RMSD computed with the Hungarian method
+
+    Notes
+    -----
+    The Hungarian algorithm is used to solve the linear assignment problem, which is
+    a minimum weight matching of the molecular graphs (bipartite). See [1]_ for details.
+
+    The linear assignment problem is solved for every element separately.
+
+    .. [1] W. J. Allen and R. C. Rizzo, *Implementation of the Hungarian Algorithm to
+        Account for Ligand Symmetry and Similarity in Structure-Based Design*,
+        J. Chem. Inf. Model. **54**, 518-529 (2014)
+    """
 
     assert A.shape == B.shape
-    assert typesA.shape == typesB.shape
+    assert atomicnumsA.shape == atomicnumsB.shape
 
-    types = set(typesA)
+    elements = set(atomicnumsA)
 
     total_cost: float = 0.0
-    for t in types:
-        typesA_idx = typesA == t
-        typesB_idx = typesB == t
+    for t in elements:
+        atomicnumsA_idx = atomicnumsA == t
+        atomicnumsB_idx = atomicnumsB == t
 
-        assert typesA_idx.shape == typesB_idx.shape
+        assert atomicnumsA_idx.shape == atomicnumsB_idx.shape
 
-        cost, row_idx, col_idx = optimal_assignment(A[typesA_idx, :], B[typesB_idx, :])
+        cost, row_idx, col_idx = optimal_assignment(
+            A[atomicnumsA_idx, :], B[atomicnumsB_idx, :]
+        )
 
         total_cost += cost
 
