790d4199e2ef4d5b5a6ef0684d7dc30aed8b8c85
Rocco Meli <r.meli@bluemail.ch>
2020-02-23 21:50:45 +0000

    make openbabel and RDKit completely optional

--- spyrmsd/io.py
+++ spyrmsd/io.py
@@ -10,22 +10,39 @@ try:
     )
 
 except ImportError:
-    from spyrmsd.optional.rdkit import (
-        load,
-        loadall,
-        adjacency_matrix,
-        to_molecule,
-        numatoms,
-        numbonds,
-        bonds,
-    )
 
-__all__ = [
-    "load",
-    "loadall",
-    "adjacency_matrix",
-    "to_molecule",
-    "numatoms",
-    "numbonds",
-    "bonds",
-]
+    try:
+        from spyrmsd.optional.rdkit import (
+            load,
+            loadall,
+            adjacency_matrix,
+            to_molecule,
+            numatoms,
+            numbonds,
+            bonds,
+        )
+    except ImportError:
+        # Use sPyRMSD as standalone library
+        __all__ = []
+    else:
+        # Avoid flake8 complaint "imported but unused"
+        __all__ = [
+            "load",
+            "loadall",
+            "adjacency_matrix",
+            "to_molecule",
+            "numatoms",
+            "numbonds",
+            "bonds",
+        ]
+else:
+    # Avoid flake8 complaint "imported but unused"
+    __all__ = [
+        "load",
+        "loadall",
+        "adjacency_matrix",
+        "to_molecule",
+        "numatoms",
+        "numbonds",
+        "bonds",
+    ]
