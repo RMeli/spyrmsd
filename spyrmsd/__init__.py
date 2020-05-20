9106dfc1b1ebc197c8479fb39491f3f4b70602dc
Rocco Meli <r.meli@bluemail.ch>
2020-05-20 18:40:33 +0200

    add package citation from zenodo

--- spyrmsd/__init__.py
+++ spyrmsd/__init__.py
@@ -2,10 +2,17 @@
 Python RMSD tool with symmetry correction.
 """
 
-# Handle versioneer
+from .due import due, Doi
 from ._version import get_versions
 
 versions = get_versions()
 __version__ = versions["version"]
 __git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
+# This will print latest Zenodo version
+due.cite(
+    Doi("10.5281/zenodo.3631876"),
+    path="spyrmsd",
+    description="spyRMSD: Symmetry-Corrected RMSD in Python",
+)
