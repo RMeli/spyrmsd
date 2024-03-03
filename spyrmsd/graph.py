bced3a3834e1d2a91c1425d601d36c3168114cf9
jnelen <jnelen@ucam.edu>
2024-03-03 19:26:49 +0100

    Use precommit hooks add __all__ back refactor _alias_backendDict to _alias_to_backend

--- spyrmsd/graph.py
+++ spyrmsd/graph.py
@@ -1,7 +1,8 @@
-from spyrmsd import constants
+import warnings
 
 import numpy as np
-import warnings
+
+from spyrmsd import constants
 
 _available_backends = []
 _current_backend = None
@@ -11,64 +12,67 @@ _graph_tool_aliases = ["graph_tool", "graphtool", "graph-tool", "graph tool", "g
 _networkx_aliases = ["networkx", "nx"]
 
 ## Construct the alias dictionary
-_alias_backendDict = {}
+_alias_to_backend = {}
 for alias in _graph_tool_aliases:
-    _alias_backendDict[alias.lower()] = "graph-tool"
+    _alias_to_backend[alias.lower()] = "graph-tool"
 for alias in _networkx_aliases:
-    _alias_backendDict[alias.lower()] = "networkx"
+    _alias_to_backend[alias.lower()] = "networkx"
 
 try:
+    from spyrmsd.graphs.gt import cycle as gt_cycle
     from spyrmsd.graphs.gt import (
-        cycle as gt_cycle,
         graph_from_adjacency_matrix as gt_graph_from_adjacency_matrix,
-        lattice as gt_lattice,
-        match_graphs as gt_match_graphs,
-        num_edges as gt_num_edges,
-        num_vertices as gt_num_vertices,
-        vertex_property as gt_vertex_property,
     )
+    from spyrmsd.graphs.gt import lattice as gt_lattice
+    from spyrmsd.graphs.gt import match_graphs as gt_match_graphs
+    from spyrmsd.graphs.gt import num_edges as gt_num_edges
+    from spyrmsd.graphs.gt import num_vertices as gt_num_vertices
+    from spyrmsd.graphs.gt import vertex_property as gt_vertex_property
+
     _available_backends.append("graph-tool")
 except ImportError:
     warnings.warn("The graph-tool backend does not seem to be installed.", stacklevel=2)
-    
+
 try:
-   from spyrmsd.graphs.nx import (
-        cycle as nx_cycle,
+    from spyrmsd.graphs.nx import cycle as nx_cycle
+    from spyrmsd.graphs.nx import (
         graph_from_adjacency_matrix as nx_graph_from_adjacency_matrix,
-        lattice as nx_lattice,
-        match_graphs as nx_match_graphs,
-        num_edges as nx_num_edges,
-        num_vertices as nx_num_vertices,
-        vertex_property as nx_vertex_property,
     )
-   _available_backends.append("networkx")
+    from spyrmsd.graphs.nx import lattice as nx_lattice
+    from spyrmsd.graphs.nx import match_graphs as nx_match_graphs
+    from spyrmsd.graphs.nx import num_edges as nx_num_edges
+    from spyrmsd.graphs.nx import num_vertices as nx_num_vertices
+    from spyrmsd.graphs.nx import vertex_property as nx_vertex_property
+
+    _available_backends.append("networkx")
 except ImportError:
-   warnings.warn("The networkx backend does not seem to be installed.", stacklevel=2)
+    warnings.warn("The networkx backend does not seem to be installed.", stacklevel=2)
 
 
 def _validate_backend(backend):
-    standardized_backend = _alias_backendDict.get(backend.lower())
+    standardized_backend = _alias_to_backend.get(backend.lower())
     if standardized_backend is None:
         raise ValueError("This backend is not recognized or supported")
     if standardized_backend not in _available_backends:
         raise ImportError(f"The {backend} backend doesn't seem to be installed")
     return standardized_backend
 
+
 def available_backends():
-    return _available_backends 
+    return _available_backends
+
 
 def set_backend(backend):
-    ## Check if the backend is valid
     backend = _validate_backend(backend)
 
     ## Check if we actually need to switch backends
     if backend == _current_backend:
         warnings.warn(f"The backend is already {backend}", stacklevel=2)
         return _current_backend
-        
+
     global cycle, graph_from_adjacency_matrix, lattice, match_graphs, num_edges, num_vertices, vertex_property
-    
-    if backend == "graph-tool":      
+
+    if backend == "graph-tool":
         cycle = gt_cycle
         graph_from_adjacency_matrix = gt_graph_from_adjacency_matrix
         lattice = gt_lattice
@@ -76,7 +80,7 @@ def set_backend(backend):
         num_edges = gt_num_edges
         num_vertices = gt_num_vertices
         vertex_property = gt_vertex_property
-              
+
     elif backend == "networkx":
         cycle = nx_cycle
         graph_from_adjacency_matrix = nx_graph_from_adjacency_matrix
@@ -84,17 +88,32 @@ def set_backend(backend):
         match_graphs = nx_match_graphs
         num_edges = nx_num_edges
         num_vertices = nx_num_vertices
-        vertex_property = nx_vertex_property      
+        vertex_property = nx_vertex_property
 
     return backend
 
+
+__all__ = [
+    "graph_from_adjacency_matrix",
+    "match_graphs",
+    "vertex_property",
+    "num_vertices",
+    "num_edges",
+    "lattice",
+    "cycle",
+    "adjacency_matrix_from_atomic_coordinates",
+]
+
 if len(_available_backends) == 0:
-    raise ImportError("No valid backends found. Please ensure that either graph-tool or NetworkX are installed.")
+    raise ImportError(
+        "No valid backends found. Please ensure that either graph-tool or NetworkX are installed."
+    )
 else:
     if _current_backend is None:
-        ## Set the backend to the first available (preferred) backend         
+        ## Set the backend to the first available (preferred) backend
         _current_backend = set_backend(backend=_available_backends[0])
-    
+
+
 def get_backend():
     return _current_backend
 
