bcff9d4a36310ab0792437e55a606d303f2ce8e8
frgoe003 <102743232+frgoe003@users.noreply.github.com>
2025-04-23 11:11:25 +0200

    Update symmrmsd isomorphism test

--- tests/test_rmsd.py
+++ tests/test_rmsd.py
@@ -389,11 +389,10 @@ def test_symmrmsd_benzene_isomorphism(benzene, angle: float, shift: int) -> None
         return_best_isomorphism=True,
     )
 
-    assert np.all(best_isomorphism[1] == list(range(12)))
-
     # Indices should be shifted by 2 (Carbon+Hydrogen) for every 60 degrees
-    expected_first_list = [(i + 2 * shift) % 12 for i in range(12)]
-    assert np.all(best_isomorphism[0] == expected_first_list)
+    excpected_isomorphism = np.roll(best_isomorphism[0], shift)
+
+    assert np.all(excpected_isomorphism == best_isomorphism[1])
 
 
 @pytest.mark.parametrize("angle", [60, 120, 180, 240, 300, 360])
